ce9b1e330ff4123f2a394d170d2a62a12d92946f
RymoswagReal <Feiner@colum.edu>
2025-04-03 23:49:26 -0500

    Feiner_Project4

---
+++ FeinerProject4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private float spawneRange = 9.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-9, 9);
+        float spawnPosZ = Random.Range(-9, 9);
+
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+
+        return randomPos;
+    }
+}
